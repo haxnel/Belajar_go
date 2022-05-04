a4e0313bd1f4c102acf5785313e9f7a0be1e6ce3
Lintang Hannan M <lintanghannan123@gmail.com>
2022-05-05 06:30:22 +0700

    awal

--- variable.go
+++ variable.go
@@ -4,10 +4,23 @@ import "fmt"
 
 func main() {
 	var name string
-	
+
 	name = "Lintang Hannan"
 	fmt.Println(name)
 
 	name = "Lintang Maulana"
 	fmt.Println(name)
+
+	var friendname = "Mawar"
+	fmt.Println(friendname)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstname = "Lintang"
+		lastname = "Maulana"
+	)
+	fmt.Println(firstname)
+	fmt.Println(lastname)
 }
