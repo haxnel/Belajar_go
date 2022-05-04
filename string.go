929589138ee016e9d49932e57855a1690af27b71
Lintang Hannan M <lintanghannan123@gmail.com>
2022-05-05 06:23:22 +0700

    awal

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Lintang")
+	fmt.Println("Lintang Hannan")
+	fmt.Println("Lintang Hannan Maulana")
+
+	fmt.Println(len("Lintang"))
+	fmt.Println("Lintang Hannan"[0])
+	fmt.Println("Lintang Hannan Maulana"[1])
+}
---
+++ variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	
+	name = "Lintang Hannan"
+	fmt.Println(name)
+
+	name = "Lintang Maulana"
+	fmt.Println(name)
+}
