bdea04c0abb8f3e4bd6a0be193db1ece6b91e537
Lintang Hannan M <lintanghannan123@gmail.com>
2022-05-05 13:00:43 +0700

    awal

---
+++ constant.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	const (
+		firstname string = "Lintang"
+		lastname = "Maulana"
+		value = 1000
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+	fmt.Println(value)
+
+}
