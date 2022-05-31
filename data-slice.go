d6dbeb123f3d5a185de38e5c7d52dbb0422928de
haxnel <lintanghannan123@gmail.com>
2022-05-31 21:25:00 +0700

    belajar

--- data-slice.go
+++ data-slice.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var month = [...]string{
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember",
+	}
+
+	var slice1 = month[4:7]
+	fmt.Println(slice1)
+	fmt.Println(len(slice1))
+	fmt.Println(cap(slice1))
+
+	//month[5] = "Diubah"
+	//fmt.Println(slice1)
+
+	//slice1[0] = "Mei lagi"
+	//fmt.Println(month)
+
+	var slice2 = month[11:]
+	fmt.Println(slice2)
+
+	var slice3 = append(slice2, "Linux")
+	fmt.Println(slice3)
+	slice3[1] = "Bukan desember"
+	fmt.Println(slice3)
+
+	fmt.Println(slice2)
+	fmt.Println(month)
+
+	//membuat slice yang aman
+	newSlice := make([]string, 2, 5)
+	newSlice[0] = "Hello"
+	newSlice[1] = "World"
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	//copy slice
+	copySlice := make([]string, len(newSlice), cap(newSlice))
+	copy(copySlice, newSlice)
+	fmt.Println(copySlice)
+
+	/* Hati - hati saat membuat array, karena nanti yang dibuat malahan slice */
 }
